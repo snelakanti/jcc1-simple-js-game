dc8618ddf2f27af640667853f14f2a009858b5bb
snelakanti <vnelakanti@gmail.com>
2018-02-23 16:00:06 -0500

    add player 1 values

---
+++ script.js
@@ -0,0 +1,31 @@
+var poneName = prompt("Name 1?"); //n1 sherlock age 20 height 183
+var oAge = prompt("poneAge?");
+var poneAge = parseInt(oAge);
+var oHeight = prompt("poneHeight?");
+var poneHeight = parseInt(oHeight);
+
+console.log(poneName + " is of " + poneAge + " years old and his height is " + " " + poneHeight);   
+
+// var ptwoName = 'John';
+// var ptwoAge = 24;
+// var ptwoHeight = 172;
+
+// var ptwoName = prompt("Name 2?");
+// var ptwoAge = prompt("ptwoAge?");
+// var ptwoHeight = prompt("ptwoHeight?");
+// console.log(ptwoName + " is of " + ptwoAge + " years old and his height is " + " " + ptwoHeight);   
+
+
+var poneValue = poneHeight + (5 * poneAge);
+console.log(poneValue);
+// var ptwoValue = ptwoHeight + (5 * ptwoAge);
+
+// if (poneValue>ptwoValue){
+//     console.log(poneName +" "+ "wins!");
+// }
+// else if (ptwoValue>poneValue){
+//     console.log(ptwoName +" "+ "wins!");
+// }
+// else{
+//     console.log("Draw!")
+// }
